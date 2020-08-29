a17ea3c358cc4d13fbc4d367ce6f9b310a3f840c
Antonio <antoniomarcos997@gmail.com>
2020-08-29 00:37:52 -0300

    first commit

---
+++ src/services/NSE.Identidade.API/Configuration/IdentityConfig.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Extensions;
+using System.Text;
+
+namespace NSE.Identidade.API.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            var appsettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appsettingsSection);
+
+            var appSettings = appsettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(bearerOptions =>
+            {
+                bearerOptions.RequireHttpsMetadata = true;
+                bearerOptions.SaveToken = true;
+                bearerOptions.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+
+                };
+            });
+            return services;
+        }
+        public static IApplicationBuilder UseIdentityConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                IdentityModelEventSource.ShowPII = true;
+            }
+            app.UseAuthentication();
+            app.UseAuthorization();
+            return app;
+        }
+    }
+}
---
+++ src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Options;
+using NSE.WebApp.MVC.Extensions;
+using NSE.WebApp.MVC.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public class AutenticacaoService : Service, IAutenticacaoService
+    {
+        private readonly HttpClient _httpClient;
+        public AutenticacaoService(HttpClient httpClient,IOptions<AppSettings> options)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new System.Uri(options.Value.AutenticacaoUrl);
+        }
+        public async Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin)
+        {
+            var loginContent = ObterConteudo(usuarioLogin);
+            var response = await _httpClient.PostAsync("/api/identidade/login",
+                                                loginContent);
+
+            if (!TratarErrosResponse(response))
+            {
+                return new UsuarioRespostaLogin
+                {
+                    ResponseResult = await DeserealizarObjetoResponse<ResponseResult>(response)
+                };
+            }
+
+            return await DeserealizarObjetoResponse<UsuarioRespostaLogin>(response);
+        }
+
+        public async Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            var registroContent = ObterConteudo(usuarioRegistro);
+            var response = await _httpClient.PostAsync("/api/identidade/nova-conta",
+                                                registroContent);
+            if (!TratarErrosResponse(response))
+            {
+                return new UsuarioRespostaLogin
+                {
+                    ResponseResult = await DeserealizarObjetoResponse<ResponseResult>(response)
+                };
+            }
+            return await DeserealizarObjetoResponse<UsuarioRespostaLogin>(response);
+        }
+    }
+}
